a6f1672d660558c64e8c8f6a5c6145fd1408c5dd
tongt0 <1498520459@qq.com>
2018-11-29 17:03:18 +0800

    'myVueInit'

---
+++ src/components/Home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <p>你好呀</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// 引入重置样式
+import './assets/style/reset.css'
+// 解决边框问题
+import './assets/style/border.css'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
